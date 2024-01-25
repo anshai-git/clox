4671e61cd051b164a5cba7f77bbfa7413a6bc486
Kovacs Ervin <kov.ervin97@gmail.com>
2024-01-25 14:37:26 +0200

    sync

--- README.md
+++ README.md
@@ -3,7 +3,6 @@ clox implementation from the book "Crafting Interpreters" by Robert Nystrom
 
 SOURCE CODE -> [SCANNER] -> TOKENS -> [COMPILER] -> BYTECODE CHUNK -> [VM]
 
-
 ## Parsing
 
 We map each token type to a different kind of expression. We define a function
@@ -11,3 +10,8 @@ for each expression that outputs the appropriate bytecode. Then we build an
 array of function pointers. The indexes in the array correspond to the
 TokenType enum values, and the function at each index is the code to compile
 an expression of that token type.
+
+Parsing strategy: **Pratt parser**
+
+A Pratt parser isn’t a recursive descent parser, but it’s still recursive.
+That’s to be expected since the grammar itself is recursive. 
--- compiler.c
+++ compiler.c
@@ -31,13 +31,15 @@ static void end_compiler();
 
 static void expression();
 static void grouping();
+static void unary();
 static void number();
 static void emit_constant(Value value);
 static uint8_t make_constant(Value value);
 
 // FE FUNCTIONS START
 static void error_at(Token* token, const char* message) {
-  if (parser.panic_mode) return;
+  if (parser.panic_mode)
+    return;
   parser.panic_mode = true;
   fprintf(stderr, "[line %d] Error", token->line);
 
@@ -53,9 +55,7 @@ static void error_at(Token* token, const char* message) {
   parser.had_error = true;
 }
 
-static void error(const char* message) {
-  error_at(&parser.previous, message);
-}
+static void error(const char* message) { error_at(&parser.previous, message); }
 
 static void error_at_current(const char* message) {
   error_at(&parser.current, message);
@@ -75,7 +75,8 @@ static void advance() {
 
   for (;;) {
     parser.current = scan_token();
-    if (parser.current.type != TOKEN_ERROR) break;
+    if (parser.current.type != TOKEN_ERROR)
+      break;
 
     error_at_current(parser.current.start);
   }
@@ -84,7 +85,8 @@ static void advance() {
 
 // BE FUNCTIONS START
 
-//  It writes the given byte, which may be an opcode or an operand to an instruction.
+//  It writes the given byte, which may be an opcode or an operand to an
+//  instruction.
 // It sends in the previous token’s line information so that runtime errors are
 // associated with that line.
 static void emit_byte(uint8_t byte) {
@@ -96,13 +98,9 @@ static void emit_bytes(uint8_t byte_1, uint8_t byte_2) {
   enit_byte(byte_2);
 }
 
-static Chunk* current_chunk() {
-  return compiling_chunk;
-}
+static Chunk* current_chunk() { return compiling_chunk; }
 
-static void end_compiler() {
-  emit_byte(OP_RETURN);
-}
+static void end_compiler() { emit_byte(OP_RETURN); }
 
 static void expression() {
   // NOT IMPLEMENTED
@@ -111,6 +109,23 @@ static void expression() {
 static void number() {
   double value = strtod(parser.previous.start, NULL);
   emit_constant(value);
+}
+
+static void unary() {
+  TokenType operator_type = parser.previous.type;
+
+  // Compile the operand
+  expression();
+
+  // Emit the operator instruction
+  switch (operator_type) {
+  case TOKEN_MINUS: {
+    emit_byte(OP_NEGATE);
+    break;
+  }
+  default:
+    return;
+  }
 }
 
 static void emit_constant(Value value) {
@@ -124,7 +139,15 @@ static uint8_t make_constant(Value value) {
     return 0;
   }
 
-  return (uint8_t) constant;
+  return (uint8_t)constant;
+}
+
+// we assume the initial ( has already been consumed. We recursively call back
+// into expression() to compile the expression between the parentheses, then
+// parse the closing ) at the end.
+static void grouping() {
+  expression();
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression");
 }
 
 // BE FUNCTIONS END
@@ -134,23 +157,26 @@ bool compile(const char* source, Chunk* chunk) {
   compiling_chunk = chunk;
 
   // SYNCHRONIZATION POINT
-  
+
   // We want to avoid error cascades.
   // If the user has a mistake in their code and the parser gets confused
-  // about where it is in the grammar, we don’t want it to spew out a whole pile of
-  // meaningless knock-on errors after the first one.
-  
-  // To solve this:
-  // We add a flag to track whether we’re currently in panic mode. (@SEE_VARIBALE :: panice_mode)
-  // When an error occurs, we set it. (@SEE_FUNCTION :: error_at)
+  // about where it is in the grammar, we don’t want it to spew out a whole pile
+  // of meaningless knock-on errors after the first one.
 
-  // After that, we go ahead and keep compiling as normal as if the error never occurred.
-  // The bytecode will never get executed, so it’s harmless to keep on trucking.
+  // To solve this:
+  // We add a flag to track whether we’re currently in panic mode.
+  // (@SEE_VARIBALE :: panice_mode) When an error occurs, we set it.
+  // (@SEE_FUNCTION :: error_at)
+
+  // After that, we go ahead and keep compiling as normal as if the error never
+  // occurred. The bytecode will never get executed, so it’s harmless to keep on
+  // trucking.
 
   // The trick is that while the panic mode flag is set, we simply suppress
   // any other errors that get detected. (@SEE_FUNCTION :: error_at)
 
-  // Panic mode ends when the parser reaches a synchronization point. (where the flags get reset)
+  // Panic mode ends when the parser reaches a synchronization point. (where the
+  // flags get reset)
   parser.had_error = false;
   parser.panic_mode = false;
 
