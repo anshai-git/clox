f1cbd0a2e6c6ca76a1a2f915a80485ef9a1ebec8
Kovacs Ervin <kov.ervin97@gmail.com>
2025-12-19 21:01:22 +0200

    last chance

--- build.sh
+++ build.sh
@@ -1,32 +1,12 @@
 # #!/bin/bash
 
 set -e
-
-# Name of the output executable
-OUTPUT="lox"
-
-# Compiler
-CC=clang
-
-# Compiler flags
-CFLAGS="-std=c99 -Wall -Wextra -O2"
-
-# Source files (order doesn't matter here, but can help readability)
 SOURCES=(
-  chunk.c
-  compiler.c
-  debug.c
   main.c
-  memory.c
-  object.c
-  scanner.c
-  table.c
-  value.c
-  vm.c
 )
 
-# Build command
 echo "Compiling..."
-$CC $CFLAGS "${SOURCES[@]}" -o $OUTPUT
+
+clang -std=c99 -Wall -Wextra -O2 "${SOURCES[@]}" -o lox
 
 echo "Build complete: ./$OUTPUT"
--- debug.h
+++
@@ -1,9 +0,0 @@
-#ifndef clox_debug_h
-#define clox_debug_h
-
-#include "chunk.h"
-
-void disassemble_chunk(Chunk* chunk, const char* name);
-int disassemble_instruction(Chunk* vhunk, int offset);
-
-#endif
